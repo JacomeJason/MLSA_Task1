c160c0c41c83f8243f36c89e160ff9c617df3685
JacomeJason <148415039+JacomeJason@users.noreply.github.com>
2023-10-25 23:50:25 -0400

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'M passionate about solving problems in new creative ways to drive innovation. By using  Power Platform Solutions.";
 
 const About = () => {
   return (
